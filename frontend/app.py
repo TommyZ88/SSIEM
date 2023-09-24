f9c36af6920f04f618616465a6fce15f9e2466f6
Faiz Ibrahim <faizibrahim95@outlook.com>
2023-09-25 09:33:53 +1000

    Created - Alerts by Severity Pie Chart

--- frontend/app.py
+++ frontend/app.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import plotly.io as pio
 from pandas import DataFrame
 import pandas as pd
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -15,54 +16,46 @@ es = Elasticsearch(['elasticsearch:9200'], # ES Connection To elastic DB
 
 pio.renderers.default = 'browser'  # set the default renderer to browser
 
-@app.route('/dashboard')
-def dashboard():
-    plot1 = create_alert_level_over_time_plot() # Query and create multiple plots here
-    plot2 = create_alerts_per_agent_plot()
-    agent_table = create_agent_info_table()  # Initiating the creation of the agent_table here
-    return render_template('dashboard.html', plot1=plot1, plot2=plot2, agent_table=agent_table)  # Render multiple templates with multiple plots
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-
-@app.route('/dashboard_data')
-def dashboard_data():
-    plot1 = create_alert_level_over_time_plot()
-    plot2 = create_alerts_per_agent_plot()
-    agent_table = create_agent_info_table()
-    return jsonify(plot1=plot1.to_html(full_html=False), plot2=plot2.to_html(full_html=False), agent_table=agent_table)
+@app.route('/test_es')
+def test_es_connection():
+    try:
+        res = es.info()
+        return jsonify(res)
+    except Exception as e:
+        return str(e)
 
 @app.route('/management') #account management page
 def management():
     return render_template('management.html')
 
+@app.route('/dashboard')
+def dashboard():
+    plot2 = create_alerts_per_agent_plot()
+    agent_table = create_agent_info_table()  
+    alert_pie_chart = create_alert_pie_chart()
+    return render_template('dashboard.html', 
+                           agent_table=agent_table,
+                           alert_pie_chart=alert_pie_chart,
+                           plot2=plot2) 
 
-def create_alert_level_over_time_plot():
-    body = {
-        "size": 0,
-        "aggs": {
-            "alerts_over_time": {
-                "date_histogram": {"field": "@timestamp", "calendar_interval": "day"},
-                "aggs": {"avg_level": {"avg": {"field": "rule.level"}}}
-            }
-        }
-    }
-    res = es.search(index="wazuh-alerts-*", body=body)
-    buckets = res['aggregations']['alerts_over_time']['buckets']
 
-    dates = [bucket['key_as_string'] for bucket in buckets]
-    levels = [bucket['avg_level']['value'] for bucket in buckets]
-
-    fig = px.line(x=dates, y=levels, labels={'x': 'Date', 'y': 'Average Alert Level'})
-    fig.update_layout(title='Alert Level Over Time')
-    fig.update_xaxes(tick0=dates[0] if dates else "2023-09-01", dtick="86400000")  # 86400000 milliseconds per day
-
-    return fig.to_html(full_html=False)
+@app.route('/dashboard_data')
+def dashboard_data():
+    plot2 = create_alerts_per_agent_plot()
+    agent_table = create_agent_info_table()
+    return jsonify(plot2=plot2.to_html(full_html=False), 
+                   agent_table=agent_table)
 
 
 def create_agent_info_table():
-    body = {"size": 1000}  # adjust the size according to your needs
+    body = {"size": 1000} 
     res = es.search(index="wazuh-monitoring-*", body=body)
     agents = res['hits']['hits']
-    seen_agent_ids = set()  # Keep track of seen agent IDs
+    seen_agent_ids = set() 
 
     table_data = []
     for agent in agents:
@@ -77,10 +70,38 @@ def create_agent_info_table():
         status = source.get("status", "")
         
         table_data.append([agent_id, name, ip, status])
-        seen_agent_ids.add(agent_id)  # Mark this agent ID as seen
+        seen_agent_ids.add(agent_id)
 
     df = DataFrame(table_data, columns=["Agent ID", "Name", "IP", "Status"])
     return df.to_html(index=False, classes='table table-striped')
+
+
+def create_alert_pie_chart():
+    today = datetime.utcnow()
+    start_of_today = today.replace(hour=0, minute=0, second=0, microsecond=0)
+    body = {
+        "size": 0,
+        "query": {
+            "range": {
+                "@timestamp": {
+                    "gte": start_of_today
+                }
+            }
+        },
+        "aggs": {
+            "severity_count": {
+                "terms": {"field": "rule.level"}
+            }
+        }
+    }
+    res = es.search(index="wazuh-alerts-*", body=body)
+    buckets = res['aggregations']['severity_count']['buckets']
+
+    labels = [str(bucket['key']) for bucket in buckets]
+    values = [bucket['doc_count'] for bucket in buckets]
+
+    fig = px.pie(values=values, names=labels)
+    return fig.to_html(full_html=False)
 
 
 def create_alerts_per_agent_plot():
@@ -134,18 +155,7 @@ def create_alerts_per_agent_plot():
     return fig.to_html(full_html=False)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
-@app.route('/test_es')
-def test_es_connection():
-    try:
-        # This line will test the connection and return Elasticsearch info
-        res = es.info()
-        return jsonify(res)
-    except Exception as e:
-        return str(e)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(5000), debug=True)
