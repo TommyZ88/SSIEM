238a567da7afddf4de59fc48797aee57d6ae8f6f
Faiz Ibrahim <faizibrahim95@outlook.com>
2023-09-25 10:00:26 +1000

    Created - Authentication Failure Bar Chart

--- frontend/app.py
+++ frontend/app.py
@@ -37,9 +37,11 @@ def dashboard():
     plot2 = create_alerts_per_agent_plot()
     agent_table = create_agent_info_table()  
     alert_pie_chart = create_alert_pie_chart()
+    auth_failure_bar_chart = create_auth_failure_bar_chart()
     return render_template('dashboard.html', 
                            agent_table=agent_table,
                            alert_pie_chart=alert_pie_chart,
+                           auth_failure_bar_chart=auth_failure_bar_chart,
                            plot2=plot2) 
 
 
@@ -47,8 +49,7 @@ def dashboard():
 def dashboard_data():
     plot2 = create_alerts_per_agent_plot()
     agent_table = create_agent_info_table()
-    return jsonify(plot2=plot2.to_html(full_html=False), 
-                   agent_table=agent_table)
+    return jsonify(plot2=plot2.to_html(full_html=False))
 
 
 def create_agent_info_table():
@@ -102,6 +103,49 @@ def create_alert_pie_chart():
 
     fig = px.pie(values=values, names=labels)
     return fig.to_html(full_html=False)
+
+
+def create_auth_failure_bar_chart():
+    body = {
+        "size": 0,
+        "aggs": {
+            "auth_failures_over_time": {
+                "date_histogram": {"field": "@timestamp", "fixed_interval": "10m"},
+                "aggs": {"auth_failures": {"terms": {"field": "rule.description", "include": ["Authentication failure"]}}}
+            }
+        }
+    }
+    res = es.search(index="wazuh-alerts-*", body=body)
+    buckets = res['aggregations']['auth_failures_over_time']['buckets']
+    
+    if not buckets:
+        return "No data available"
+    
+    start_time = datetime.fromisoformat(buckets[0]['key_as_string'].replace('Z', '+00:00'))
+    end_time = datetime.fromisoformat(buckets[-1]['key_as_string'].replace('Z', '+00:00'))
+    
+    time_intervals = []
+    current_time = start_time
+    while current_time <= end_time:
+        time_intervals.append(current_time)
+        current_time += timedelta(minutes=15)
+
+    timestamps = []
+    counts = []
+    for time_interval in time_intervals:
+        bucket = next((bucket for bucket in buckets if datetime.fromisoformat(bucket['key_as_string'].replace('Z', '+00:00')) == time_interval), None)
+        
+        timestamps.append(time_interval.isoformat())
+        if bucket:
+            counts.append(bucket['doc_count'])
+        else:
+            counts.append(0)  
+    
+    fig = px.bar(x=timestamps, y=counts, labels={'x': 'Time', 'y': 'Failures Count'})
+    fig.update_layout(title='Authentication Failures Over Time')
+    return fig.to_html(full_html=False)
+
+
 
 
 def create_alerts_per_agent_plot():
