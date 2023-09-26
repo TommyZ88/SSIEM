f0997d5ca66b6dfa301d47056dc961b48b9bd80e
Rhyanna <rhyannazaharom@gmail.com>
2023-09-27 05:41:30 +1000

    Added comments to event_logs_table.py

--- frontend/visualisations/event_logs_table.py
+++ frontend/visualisations/event_logs_table.py
@@ -1,26 +1,36 @@
 from elasticsearch import Elasticsearch
 from pandas import DataFrame
 from datetime import datetime
-import time
 
 def create_event_logs_table(es: Elasticsearch):
+    # Define the search query to retrieve 1000 log entries, sorted by timestamp in descending order
     body = {
         "size": 1000,
         "query": {"match_all": {}},
         "sort": [{"@timestamp": "desc"}]
     }
+    
+    # Execute the search query on the Elasticsearch index "wazuh-alerts-*"
     res = es.search(index="wazuh-alerts-*", body=body)
+    
+    # Extract the log entries from the search results
     logs = res['hits']['hits']
 
     table_data = []
+    # Iterate over each log entry to extract relevant information
     for log in logs:
         source = log["_source"]
+        
+        # Parse the timestamp into a datetime object, if available
         timestamp = source.get("@timestamp")
         if timestamp:
             timestamp = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
+        
+        # Extract agent name and IP address from the log entry
         agent_name = source.get("agent", {}).get("name", "")
         agent_ip = source.get("agent", {}).get("ip", "")
         
+        # Append the extracted information to the table_data list
         table_data.append({
             "Agent Name": agent_name,
             "IP Address": agent_ip,
@@ -29,5 +39,6 @@ def create_event_logs_table(es: Elasticsearch):
             "Severity": source.get("rule", {}).get("level", "")
         })
     
-    df = DataFrame(table_data, columns=["Agent Name", "IP Address","Timestamp", "Event", "Severity"])
+    # Convert the table_data list to a DataFrame and then to an HTML table
+    df = DataFrame(table_data, columns=["Agent Name", "IP Address", "Timestamp", "Event", "Severity"])
     return df.to_html(index=False, classes='table table-striped')
