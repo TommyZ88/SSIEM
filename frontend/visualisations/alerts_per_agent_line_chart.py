4567d828f5ee6cf023ff70f2cf8a3b5e40539369
Rhyanna <rhyannazaharom@gmail.com>
2023-09-27 06:33:39 +1000

    Updated Number of Alerts per Agent visualisation

--- frontend/visualisations/alerts_per_agent_line_chart.py
+++ frontend/visualisations/alerts_per_agent_line_chart.py
@@ -1,7 +1,6 @@
 from elasticsearch import Elasticsearch
 import plotly.express as px
 import pandas as pd
-from datetime import datetime
 
 def create_alerts_per_agent_line_chart(es: Elasticsearch):
     
@@ -9,8 +8,13 @@ def create_alerts_per_agent_line_chart(es: Elasticsearch):
     body = {
         "size": 0,
         "aggs": {
-            "events_by_type": {
-                "terms": {"field": "event.type.keyword", "size": 10}
+            "alerts_per_agent": {
+                "terms": {"field": "agent.id.keyword", "size": 10},
+                "aggs": {
+                    "alerts_over_time": {
+                        "date_histogram": {"field": "@timestamp", "calendar_interval": "day"}
+                    }
+                }
             }
         }
     }
@@ -19,30 +23,32 @@ def create_alerts_per_agent_line_chart(es: Elasticsearch):
     res = es.search(index="wazuh-alerts-*", body=body)
     
     # Extract the buckets from the aggregation results
-    buckets = res['aggregations']['events_by_type']['buckets']
+    buckets = res['aggregations']['alerts_per_agent']['buckets']
     
-    # Initialize data dictionary to hold event_types and counts
+    # Initialize data dictionary to hold agent_id, dates, and counts
     data = {
-        "event_type": [],
+        "agent_id": [],
+        "dates": [],
         "counts": []
     }
     
-    # Iterate through each bucket and extract event_type and counts
+    # Iterate through each bucket and extract agent_id, dates, and counts
     for bucket in buckets:
-        event_type = bucket['key']
-        count = bucket['doc_count']
+        agent_id = bucket['key']
+        dates = [item['key_as_string'] for item in bucket['alerts_over_time']['buckets']]
+        counts = [item['doc_count'] for item in bucket['alerts_over_time']['buckets']]
         
-        # Append the extracted data to the lists in the data dictionary
-        data["event_type"].append(event_type)
-        data["counts"].append(count)
+        # Extend the lists in the data dictionary with the extracted data
+        data["agent_id"].extend([agent_id] * len(dates))
+        data["dates"].extend(dates)
+        data["counts"].extend(counts)
     
     # Convert the data dictionary to a pandas DataFrame
     df = pd.DataFrame(data)
     
-    # Create a bar chart using plotly.express
-    fig = px.bar(df, x='event_type', y='counts', 
-                 title='Number of Events by Event Type',
-                 labels={'event_type': 'Event Type', 'counts': 'Number of Events'})
+    # Create a line plot using plotly.express
+    fig = px.line(df, x='dates', y='counts', color='agent_id', title='Number of Alerts per Agent Over Time',
+                  labels={'dates': 'Date', 'counts': 'Number of Alerts'})
     
     # Return the plot as an HTML string
     return fig.to_html(full_html=False)
