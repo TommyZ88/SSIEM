25428598b2698a2fe9464d6dbf335903536c7ce0
Tommy Z <80445654+Tangu-Z@users.noreply.github.com>
2023-10-09 15:55:25 +1100

    Implementation of Login Page with all other pages requiring Login if user is not logged in

--- frontend/app.py
+++ frontend/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify, render_template, url_for, redirect, flash, session
+from flask import Flask, request, session, jsonify, render_template, url_for, redirect, flash, request, g
 from flask_wtf import FlaskForm
 from flask_bootstrap import Bootstrap
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Length
 from elasticsearch import Elasticsearch
-from flask_login import LoginManager, UserMixin, login_required
+
+import os
 
 
 import plotly.io as pio
@@ -22,12 +23,10 @@ from data.login_data import authenticate_user
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'SecretKey'
-app.config['SESSION_TYPE'] = 'filesystem'  # Use filesystem session
-app.config['SESSION_PERMANENT'] = False
+
+app.secret_key = os.urandom(24)
+
 Bootstrap(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
 
 es = Elasticsearch(['elasticsearch:9200'], # ES Connection To elastic DB
                    use_ssl=True,    
@@ -40,17 +39,21 @@ pio.renderers.default = 'browser'  # set the default renderer to browser
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = StringField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=5, max=80)])
     remember = BooleanField('Remember Me')
 
 #Home page
 @app.route('/home') 
 def home():
-    return render_template('home.html')
+    if g.user:
+        return render_template('home.html', user=session['username'])
+    return redirect(url_for('login'))
 
 @app.route("/addUser")
 def addUser():
-    return render_template("addUser.html")
+    if g.user:
+        return render_template('addUser.html', user=session['username'])
+    return redirect(url_for('login'))
 
 
 #Login Page
@@ -58,96 +61,108 @@ def addUser():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
+        session.pop('username', None)
         username = form.username.data
-        password = form.password.data
+        password = form.password.data   
         if authenticate_user(es, username, password):
             session['username'] = username
-            flash('Login successful.', 'success')
             return redirect(url_for('dashboard'))
         else:
             flash('Invalid username or password', 'danger')
     return render_template('login.html', form=form)
 
 
-@app.route('/test_es')
-def test_es_connection():
-    try:
-        res = es.info()
-        return jsonify(res)
-    except Exception as e:
-        return str(e)
+@app.before_request
+def before_request():
+    g.user = None
+    if 'username' in session:
+        g.user = session['username']
+
+@app.route('/logout')
+def logout():
+    form = LoginForm()
+    session.pop('username', None)
+    return render_template('login.html', form=form)
 
 
 #Account management page
 @app.route('/management') 
 def management():
-    return render_template('management.html')
+    if g.user:
+        return render_template('management.html', user=session['username'])
+    return redirect(url_for('login'))
 
 
 @app.route('/dashboard')
 def dashboard():
-    agent_info_table = create_agent_info_table(es)
 
-    alert_severity_pie_chart = create_alert_severity_pie_chart(es)
-    top_events_donut_chart = create_top_events_donut_chart(es)
-    mitre_attacks_donut_chart = create_top_mitre_attacks_donut_chart(es)
+    if g.user:
+        agent_info_table = create_agent_info_table(es)
 
-    distribution_alert_severity_line_graph = create_distribution_alert_severity_line_graph(es)
-    auth_failures_histogram = create_auth_failures_histogram(es)
-    total_agent_alerts_bar_graph = create_total_agent_alerts_bar_graph(es)
+        alert_severity_pie_chart = create_alert_severity_pie_chart(es)
+        top_events_donut_chart = create_top_events_donut_chart(es)
+        mitre_attacks_donut_chart = create_top_mitre_attacks_donut_chart(es)
 
-    alerts_per_agent_area_chart = create_alerts_per_agent_area_chart(es) 
+        distribution_alert_severity_line_graph = create_distribution_alert_severity_line_graph(es)
+        auth_failures_histogram = create_auth_failures_histogram(es)
+        total_agent_alerts_bar_graph = create_total_agent_alerts_bar_graph(es)
 
-    event_logs_table = create_event_logs_table(es)
-    
-    
-    return render_template('dashboard.html',
-                           agent_info_table = agent_info_table,
-                           
-                           alert_severity_pie_chart = alert_severity_pie_chart,
-                           top_events_donut_chart = top_events_donut_chart,
-                           mitre_attacks_donut_chart = mitre_attacks_donut_chart,
+        alerts_per_agent_area_chart = create_alerts_per_agent_area_chart(es) 
 
-                           distribution_alert_severity_line_graph = distribution_alert_severity_line_graph, 
-                           auth_failures_histogram = auth_failures_histogram,
-                           total_agent_alerts_bar_graph = total_agent_alerts_bar_graph,
+        event_logs_table = create_event_logs_table(es)
 
-                           alerts_per_agent_area_chart=alerts_per_agent_area_chart,
-                           
-                           event_logs_table = event_logs_table
-                           )
+
+        return render_template('dashboard.html',
+                               agent_info_table = agent_info_table,
+
+                               alert_severity_pie_chart = alert_severity_pie_chart,
+                               top_events_donut_chart = top_events_donut_chart,
+                               mitre_attacks_donut_chart = mitre_attacks_donut_chart,
+
+                               distribution_alert_severity_line_graph = distribution_alert_severity_line_graph, 
+                               auth_failures_histogram = auth_failures_histogram,
+                               total_agent_alerts_bar_graph = total_agent_alerts_bar_graph,
+
+                               alerts_per_agent_area_chart=alerts_per_agent_area_chart,
+
+                               event_logs_table = event_logs_table, user=session['username']
+                               )
+    return redirect(url_for('login'))
 
 @app.route('/dashboard_data')
 def dashboard_data():
-    agent_info_table = create_agent_info_table(es)
-  
-    alert_severity_pie_chart = create_alert_severity_pie_chart(es)
-    top_events_donut_chart = create_top_events_donut_chart(es)
-    mitre_attacks_donut_chart = create_top_mitre_attacks_donut_chart(es)
 
-    distribution_alert_severity_line_graph = create_distribution_alert_severity_line_graph(es)
-    auth_failures_histogram = create_auth_failures_histogram(es)
-    total_agent_alerts_bar_graph = create_total_agent_alerts_bar_graph(es)
-
-    alerts_per_agent_area_chart = create_alerts_per_agent_area_chart(es) 
-
-    event_logs_table = create_event_logs_table(es)
+    if g.user:    
+        agent_info_table = create_agent_info_table(es)
     
-    return jsonify(
-        agent_info_table=agent_info_table,
+        alert_severity_pie_chart = create_alert_severity_pie_chart(es)
+        top_events_donut_chart = create_top_events_donut_chart(es)
+        mitre_attacks_donut_chart = create_top_mitre_attacks_donut_chart(es)
 
-        alert_severity_pie_chart=alert_severity_pie_chart,
-        top_events_donut_chart = top_events_donut_chart,
-        mitre_attacks_donut_chart = mitre_attacks_donut_chart,
+        distribution_alert_severity_line_graph = create_distribution_alert_severity_line_graph(es)
+        auth_failures_histogram = create_auth_failures_histogram(es)
+        total_agent_alerts_bar_graph = create_total_agent_alerts_bar_graph(es)
 
-        distribution_alert_severity_line_graph = distribution_alert_severity_line_graph,
-        auth_failures_histogram = auth_failures_histogram,
-        total_agent_alerts_bar_graph = total_agent_alerts_bar_graph,
+        alerts_per_agent_area_chart = create_alerts_per_agent_area_chart(es) 
 
-        alerts_per_agent_area_chart = alerts_per_agent_area_chart,
+        event_logs_table = create_event_logs_table(es)
 
-        event_logs_table=event_logs_table
-    )
+        return jsonify(
+            agent_info_table=agent_info_table,
+
+            alert_severity_pie_chart=alert_severity_pie_chart,
+            top_events_donut_chart = top_events_donut_chart,
+            mitre_attacks_donut_chart = mitre_attacks_donut_chart,
+
+            distribution_alert_severity_line_graph = distribution_alert_severity_line_graph,
+            auth_failures_histogram = auth_failures_histogram,
+            total_agent_alerts_bar_graph = total_agent_alerts_bar_graph,
+
+            alerts_per_agent_area_chart = alerts_per_agent_area_chart,
+
+            event_logs_table=event_logs_table, user=session['username']
+        )
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(5000), debug=True)
